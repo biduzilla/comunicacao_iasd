bafc8f11c66ebdb042273997a5ddc47b24511f87
biduzilla <rlyowlly19@gmail.com>
2023-04-13 16:22:25 -0300

    layout form tarefa

---
+++ app/src/main/java/com/toddy/comunicacaodemandas/ui/activity/FormAnuncioActivity.kt
@@ -0,0 +1,12 @@
+package com.toddy.comunicacaodemandas.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.toddy.comunicacaodemandas.R
+
+class FormAnuncioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_anuncio)
+    }
+}
--- app/src/main/java/com/toddy/comunicacaodemandas/ui/activity/MainActivity.kt
+++ app/src/main/java/com/toddy/comunicacaodemandas/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.toddy.comunicacaodemandas
+package com.toddy.comunicacaodemandas.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.toddy.comunicacaodemandas.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
